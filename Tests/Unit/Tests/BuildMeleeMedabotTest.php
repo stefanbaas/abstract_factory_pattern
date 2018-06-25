b850b22e856dc5f079b3b3d2b12a8e480168a08e
Stefan Blaas <stefanbaasweb@gmail.com>
2018-06-25 13:26:21 +0200

    successful test - testInstantiation - BuildMeleeMedabotTest

---
+++ Tests/Unit/Tests/BuildMeleeMedabotTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+use App\Classes\BuildMedabot;
+use App\Classes\BuildMeleeMedabot;
+use PHPUnit\Framework\TestCase;
+
+class BuildMeleeMedabotTest extends TestCase
+{
+    private $sut;
+
+    protected function setUp() /* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+        $this->sut = new BuildMeleeMedabot();
+    }
+
+    public function testInstantiation () : void
+    {
+        $this->assertNotNull($this->sut);
+    }
+
+}
