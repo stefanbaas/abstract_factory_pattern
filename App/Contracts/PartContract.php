a107a07022d7c2245a1eb471bd0a2bfbbc887195
Stefan Blaas <stefanbaasweb@gmail.com>
2018-07-05 10:32:04 +0200

    succesful test - testInstantiation - MedabotPartTest

---
+++ App/Contracts/PartContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Contracts;
+
+interface PartContract
+{
+    public function toString() :string;
+}
---
+++ Tests/Unit/Tests/MedabotPartTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests;
+
+use App\Classes\MedabotPart;
+use PHPUnit\Framework\TestCase;
+
+class MedabotPartDouble extends MedabotPart
+{
+
+}
+
+class MedabotPartTest extends TestCase
+{
+    private $sut;
+
+    protected function setUp() /* The :void return type declaration that should be here would cause a BC issue */
+    {
+        parent::setUp();
+        $this->sut = new MedabotPartDouble();
+    }
+
+    public function testInstantiation () : void
+    {
+        $this->assertNotNull($this->sut);
+    }
+
+    /*public function testInstanceOf () : void
+    {
+        $this->assertInstanceOf(MedabotPart::class, $this->sut);
+    }
+
+    public function testToString () : void
+    {
+        $this->assertTrue(is_string($this->sut->toString()));
+    }*/
+}
